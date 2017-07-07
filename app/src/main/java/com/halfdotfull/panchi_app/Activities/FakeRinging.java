91c00e913116ef80a4ba3733ad25108590ad9f96
NexFlare <harshmagarwal@outlook.com>
2017-07-07 11:57:30 +0530

    Added fake call on lock screen

--- app/src/main/java/com/halfdotfull/panchi_app/Activities/FakeRinging.java
+++ app/src/main/java/com/halfdotfull/panchi_app/Activities/FakeRinging.java
@@ -1,31 +1,61 @@
 package com.halfdotfull.panchi_app.Activities;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.halfdotfull.panchi_app.R;
 
-public class FakeRinging extends AppCompatActivity {
+public class FakeRinging extends AppCompatActivity{
     private String networkCarrier;
     private MediaPlayer mp;
+    private Ringtone ringtone;
+    BroadcastReceiver mBroadcastReceiver;
+    boolean screenOn=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         setContentView(R.layout.activity_fake_ringing);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         TextView fakeName = (TextView)findViewById(R.id.chosenfakename);
         TextView fakeNumber = (TextView)findViewById(R.id.chosenfakenumber);
+        mBroadcastReceiver=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+                    screenOn=false;
+                    try {
+                        ringtone.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(intent.getAction().equals(Intent.ACTION_SCREEN_ON)){
+                    screenOn=true;
+                    ringtone.play();
+                }
+            }
+        };
+        IntentFilter filter=new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(mBroadcastReceiver,filter);
 
         final TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         networkCarrier = tm.getNetworkOperatorName();
@@ -44,8 +74,9 @@ public class FakeRinging extends AppCompatActivity {
         fakeNumber.setText(callNumber);
 
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-        mp = MediaPlayer.create(getApplicationContext(), notification);
-        mp.start();
+        ringtone=RingtoneManager.getRingtone(getApplicationContext(),notification);
+        if(screenOn==true)
+            ringtone.play();
 
         ImageView answerCall = (ImageView) findViewById(R.id.answercall);
         ImageView rejectCall = (ImageView) findViewById(R.id.rejectcall);
@@ -54,7 +85,8 @@ public class FakeRinging extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                mp.stop();
+                ringtone.stop();
+                unregisterReceiver(mBroadcastReceiver);
                 Intent intent =new Intent(FakeRinging.this,FakeReceiving.class);
                 startActivity(intent);
                 finish();
@@ -64,7 +96,8 @@ public class FakeRinging extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                mp.stop();
+                unregisterReceiver(mBroadcastReceiver);
+                ringtone.stop();
                 finish();
             }
         });
--- app/src/main/java/com/halfdotfull/panchi_app/Services/ScreenReceiver.java
+++ app/src/main/java/com/halfdotfull/panchi_app/Services/ScreenReceiver.java
@@ -3,6 +3,8 @@ package com.halfdotfull.panchi_app.Services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.Toast;
 
 /**
  * Created by anant bansal on 6/4/2017.
@@ -10,16 +12,21 @@ import android.content.Intent;
 
 public class ScreenReceiver extends BroadcastReceiver {
 
-    public static boolean screenOn;
+    public static boolean screenOn=true;
+    IntentFilter filter=new IntentFilter(Intent.ACTION_SCREEN_ON);
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
             screenOn = false;
         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
             screenOn = true;
+            Toast.makeText(context, "Screen On", Toast.LENGTH_SHORT).show();
         }
         /*Intent i = new Intent(context, MessageService.class);
         i.putExtra("screen_state", screenOn);
         context.startService(i);*/
     }
+
+
 }
